5141325b10de17085ff0ac04ca5da030004bbe1e
Matias <mgcm03@hotmail.com>
2025-02-26 15:33:57 -0300

    01- Signal

---
+++ 01-bases/src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+
+
+
+@Component({
+  templateUrl:'./counter-page.component.html',
+
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+
+export class CounterPageComponent {
+  counter = 10;
+  counterSignal = signal(10);
+
+  constructor(){
+    setInterval( () => {
+      this.counterSignal.update( (v) => v + 1);
+      console.log('Tick');
+    }, 2000)
+  }
+
+  increaseBy(value: number){
+
+    this.counter += value;
+
+    this.counterSignal.update((current) => current + value);
+
+  }
+
+  decreaseBy(value: number){
+    this.counter -= value;
+
+    this.counterSignal.update((current) => current - value);
+
+  }
+
+  resetCounter(){
+    this.counter = 0;
+    this.counterSignal.set(0)
+  };
+
+}
